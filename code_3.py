2e3bc6bb18d6a53167ee1a49ed3d2741dc3e41d6
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2023-11-11 23:14:15 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        int[] data = new int[10];
+}
+        }
+
+        for (int item : data) {
+        }
+    }
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+
+        for (int i = 0; i < 10; i++) {
